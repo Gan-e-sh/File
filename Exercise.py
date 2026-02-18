db3d053373836964a879ea8cdf3d6a3223097747
Gan-e-sh <ganesh.y-29@scds.saiuniversity.edu.in>
2026-02-18 12:50:33 +0530

    Add files via upload

---
+++ Exercise.py
@@ -0,0 +1,124 @@
+# exercise-1:
+# a=int(input("enter the radius"))
+# area=3.14*a*a
+# print(area)
+
+# a=int(input("enter a value"))
+# b=int(input("enter b value"))
+# print(f"addition={a+b}")
+# print(f"sub={a-b}")
+# print(f"mul={a*b}")
+# if b<0:
+#     print(f"div={a/b}")
+# else :
+#     print("not possible")
+
+# a=int(input("enter a value"))
+# c=input("enter +-*/")
+# b=int(input("enter b value"))
+# if c=='+':
+#     print(f"add{a+b}")
+# elif c=='-':
+#     print(f"sub{a-b}")
+# elif c=='*':
+#     print(f"mul{a*b}")
+# elif c=='/':
+#     if b<0:
+#         print(f"div{a/b}")
+#     else:
+#         print("not possile")
+# else:
+#     print("invalid")
+# l=float(input("enter a length"))
+# b=float(input("enter a breadth"))
+# per=2*(l+b)
+# area=l*b
+# print(f"area={area}")
+# print(f"pre={per}")
+# a=int(input("enter a num"))
+# if a<0:
+#     print("positive")
+# elif a<0:
+#     print("negative")
+# else:
+#     print("zero")
+# a=int(input("enter a num"))
+# b=int(input("enter a num"))
+# if a==b:
+#     print("equal")
+# elif a<b:
+#     print("a is less than b")
+# else:
+#     print("a is small")
+# a=int(input("enter a value"))
+# b=int(input("enter a value"))
+# c=int(input("enter a value"))
+# if a<b:
+#     if a<c:
+#         print("a is small")
+#     elif a<c:
+#         print("c is small")
+#     elif a<b and b==c:
+#         print("a is small")
+# elif b<c:
+#     if b<a:
+#         print("b is small")
+#     elif a<b:
+#         print("a is small")
+#     elif b<c and a==c:
+#         print("b is small")
+# else :
+#     if c<b:
+#         print("c is small")
+#     elif b<c:
+#         print("b is small")
+#     elif c<a and b==a:
+#         print("c is small")
+# a=int(input("enter a number"))
+# b=0
+# sum=0
+# while(a!=0){
+#     b=a%10
+#     a=a/10
+#     sum=sum+b
+# }
+# print("sum=%d",sum)    doubt
+# a=float(input("enter a number"))
+# b=float(input("enter a number"))
+# c=float(input("enter a number"))
+# d=float(input("enter a number"))
+# e=float(input("enter a number"))
+# sum=a+b+c+d+e
+# average=sum/5.0
+# print(f"sum={sum}")
+# print(f"average={average}")
+# a=int(input("enter a value"))
+# b=int(input("enter a value"))
+# c=int(input("enter a value"))
+# if a<b:
+#     if a<c:
+#         print("a is small")
+#     elif a>c:
+#         print("c is small")
+#     elif a<b and b==c:
+#         print("a is small")
+# elif b<c:
+#     if b<a:
+#         print("b is small")
+#     elif a<b:
+#         print("a is small")
+#     elif b<c and a==c:
+#         print("b is small")
+# else :
+#     if c<b:
+#         print("c is small")
+#     elif b<c:
+#         print("b is small")
+#     elif c<a and b==a:
+#         print("c is small")
+# a=int(input("enter a number"))
+# if a%2==0:
+#     print("a is even")
+# else:
+#     print("a is odd")
+
